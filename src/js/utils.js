85d0972abfd9cafa4ee58c81bbccbac6fd5731da
Pavel Chekalin <grokki91@gmail.com>
2023-02-06 23:45:03 +0300

    Add calcTileType in utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -24,6 +24,23 @@
  * */
 export function calcTileType(index, boardSize) {
   // TODO: ваш код будет тут
+  if (index === 0) {
+    return 'top-left';
+  } if (index === boardSize - 1) {
+    return 'top-right';
+  } if (index === ((boardSize ** 2) - boardSize)) {
+    return 'bottom-left';
+  } if (index === boardSize ** 2 - 1) {
+    return 'bottom-right';
+  } if (index > 0 && index < boardSize) {
+    return 'top';
+  } if (index > (boardSize - 1) && index % boardSize === 0) {
+    return 'left';
+  } if (index > ((boardSize ** 2) - boardSize) && (index < boardSize ** 2 - 1)) {
+    return 'bottom';
+  } if ([15, 23, 31, 39, 47, 55].includes(index)) {
+    return 'right';
+  }
   return 'center';
 }
 
